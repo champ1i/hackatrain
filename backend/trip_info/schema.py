b2109364861c30ce6004cb2ca392b31811a90351
Laurens Rouw <lrouw@cfp.nl>
2019-07-01 23:42:52 +0200

    Add capacities and randomized availabilities

--- backend/trip_info/schema.py
+++ backend/trip_info/schema.py
@@ -1,3 +1,6 @@
+from random import randint
+
+import graphene
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
@@ -13,6 +16,19 @@ class TripNode(DjangoObjectType):
             'destination': ['exact', 'istartswith']
         }
         interfaces = (relay.Node, )
+
+    available_first_class = graphene.Int()
+    available_second_class = graphene.Int()
+
+    def resolve_available_first_class(self, info):
+        if self is not None:
+            return self.capacity_first_class - randint(10, 60)
+        return 0
+
+    def resolve_available_second_class(self, info):
+        if self is not None:
+            return self.capacity_second_class - randint(70, 200)
+        return 0
 
 
 class Query(object):
