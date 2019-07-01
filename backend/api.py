9f27c67ed03907d9fa5eaf950450e8a0f66cf4aa
Bas Poot <cbpoot@gmail.com>
2019-07-01 23:51:57 +0200

    Added interface to NS API

---
+++ backend/api.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+
+class NS(object):
+
+    def __init__(self):
+        self.api_key = 'EURu8A0iTU7Vr2Vk524qL4U2NowHC8xYTFuTXIAc'
+
+    def get_zitplaatsen(self, treinnummer=3031):
+        query_zitplaatsen = 'https://ns-api.nl/virtualtrain/api/v1/trein/{}?features=zitplaats'.format(treinnummer)
+
+        header={'x-api-key': str(self.api_key)}
+        r = requests.get(query_zitplaatsen, headers=header)
+        data = json.loads(r.text)
+        zitplaatsEersteKlas = data['materieeldelen'][0]['zitplaatsen']['zitplaatsEersteKlas']
+        zitplaatsTweedeKlas = data['materieeldelen'][0]['zitplaatsen']['zitplaatsTweedeKlas']
+
+        return data['materieeldelen'][0]['zitplaatsen']
