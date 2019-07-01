235b1c1d8f36d20034d6d40b862f58cb8c46d25f
Laurens Rouw <lrouw@cfp.nl>
2019-07-01 21:00:55 +0200

    Update trips model and add fixture

--- backend/trip_info/models.py
+++ backend/trip_info/models.py
@@ -3,5 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Trip(models.Model):
-    start = models.CharField(max_length=255)
-    stop = models.CharField(max_length=255)
+    departure_time = models.CharField(max_length=255)
+    arrival_time = models.CharField(max_length=255)
+    origin = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    first_class_normal_price = models.FloatField()
+    first_class_spot_price = models.FloatField()
+    second_class_normal_price = models.FloatField()
+    second_class_spot_price = models.FloatField()
