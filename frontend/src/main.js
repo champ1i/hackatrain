4490083b48ae6f077a26daf8d6b7458cf58c3f0d
Bart Hettema <barthettema@gmail.com>
2019-07-02 09:36:56 +0200

    Added seating view

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from "./router.js";
 import store from "./store.js";
 import apolloProvider from './apollo'
